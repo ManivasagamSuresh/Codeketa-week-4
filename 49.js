06ddc58c59e9d9648ce089221a45c5195aac351c
manivasagam <s.kishore123.64@gmail.com>
2022-09-04 18:49:14 +0530

    added

---
+++ 49.js
@@ -0,0 +1,26 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+let a=userInput[0].split(' ').map(num=>Number(num));
+// let max = a.indexOf(Math.max(...a))
+// console.log(max)
+if(a[0]+a[1]>a[2] && a[0]+a[2]>a[1] && a[1]+a[2]>a[0])
+{console.log("yes");}
+else{console.log("no");}
+
+  //end-here
+});
